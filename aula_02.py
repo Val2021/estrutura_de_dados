4246ed7c5e58c31b0c621f9d6690ceac143e3a0f
Val Araujo <val.araujo@businessplanner.com.br>
2021-07-06 18:23:17 -0300

    implementado algoritmo de maximo

---
+++ aula_02.py
@@ -0,0 +1,22 @@
+from math import inf
+from time import time
+
+
+def meu_max(interavel):
+    numero_maximo = -inf
+    for numero in interavel:
+        if numero > numero_maximo:
+            numero_maximo = numero
+    return numero_maximo
+
+
+if __name__ == '__main__':
+    print(meu_max([1]))
+    print(meu_max([1,100]))
+    inicio = 1000000
+    for n in range(0, inicio * 20 + 1,inicio):
+        inicio = time()
+        meu_max(range(n))
+        fim = time()
+        tempo_de_execucao_em_segundos = fim-inicio
+        print('*' * int(tempo_de_execucao_em_segundos*10),n)
